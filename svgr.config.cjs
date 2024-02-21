aa4fe4ab6c9014a0883cb386bd3c401074b80c6d
Kevin Inzelrac <kevininzelrac@me.com>
2024-02-21 19:05:00 +0100

    svgr / svgo config

---
+++ svgr.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx", "@svgr/plugin-prettier"],
+  typescript: true,
+  jsxRuntime: "automatic",
+  //replaceAttrValues: { "": "currentColor" },
+};
